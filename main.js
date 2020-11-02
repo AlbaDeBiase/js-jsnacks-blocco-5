31230788f3858838d4e802baeb3d6710d85bcb19
Alba De biase <debiasealba@gmail.com>
2020-11-02 20:17:10 +0100

    js-2

---
+++ main.js
@@ -0,0 +1,33 @@
+// JSnack2: Creare un array di nomi e chiedere all'utente due numeri.
+// Creare un nuovo array con i valori che si trovano in una posizione compresa tra i due numeri inseriti dall'utente.
+
+
+
+
+
+$(document).ready(function() {
+// creo un array di nomi
+        var nomi = ['Alba', 'Dario', 'Isabella','Maria'];
+
+    // console.log(nomi);
+// chiedo all'utente 2 numeri
+        var numero1 = parseInt(prompt('Inserisci un numero'));
+        var numero2 = parseInt(prompt('Inserisci un altro numero'));
+
+        console.log(numero1);
+        console.log(numero2);
+
+// se il numero corrente si trova tra numero 1 e numero 2 stampo e inserisco in nuovo array.
+
+//         var indice = 0;
+//     for (var i = 0; i < nomi.length; i++) {
+//         nomeCorrente=nomi[i]
+//         console.log(nomeCorrente);
+//
+// }
+//
+//     if (indice == numero1 || numero2) {
+//         console.log(indice);
+// }
+
+})
